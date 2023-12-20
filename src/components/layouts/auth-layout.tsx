af16bc396bd3122d30b23f876ea037a7e19c4773
Somtochukwu Nkemakolam <sewkito@gmail.com>
2023-12-20 23:04:10 +0100

    update

--- src/components/layouts/auth-layout.tsx
+++ src/components/layouts/auth-layout.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { Logo } from "..";
 
 const AuthLayout = () => {
   return (
     <>
+      <div className="fixed top-0 w-full py-4 px-4 sm:px-12">
+        <Logo />
+      </div>
       <Outlet />
     </>
   );
