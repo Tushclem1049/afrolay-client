ab3d3b80f76a87ad56af629ddd7070645a305b71
Somtochukwu Nkemakolam <sewkito@gmail.com>
2023-12-21 15:16:51 +0100

    test

--- sdk/api/config/index.ts
+++ sdk/api/config/index.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const BASE_URL =
-// "https://dashboard-01-server-4fd151ce1921.herokuapp.com/api/v1";
+const BASE_URL =
+  "https://dashboard-01-server-4fd151ce1921.herokuapp.com/api/v1";
 
-const BASE_URL = "http://localhost:8000/api/v1";
+// const BASE_URL = "http://localhost:8000/api/v1";
 
 export default axios.create({
   baseURL: BASE_URL,
