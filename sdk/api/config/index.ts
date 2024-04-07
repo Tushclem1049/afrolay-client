1527855bac5514f2447092d21529900c698f9013
S0mt0 <sewkito@gmail.com>
2024-04-07 13:58:03 +0100

    update

--- sdk/api/config/index.ts
+++ sdk/api/config/index.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  "https://dashboard-01-server-4fd151ce1921.herokuapp.com/api/v1";
+const BASE_URL = "https://afrolay-server-c9bb6c205b41.herokuapp.com/api/v1";
 
 // const BASE_URL = "http://localhost:8000/api/v1";
 
